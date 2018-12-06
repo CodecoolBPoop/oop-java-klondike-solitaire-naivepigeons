b656b03027eb134d03b5aee6c3554f68ebdcdd7c
Akos Kolloros <kollorosa@gmail.com>
2018-12-06 12:41:38 +0100

    Restart button

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -1,9 +1,13 @@
 package com.codecool.klondike;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class Klondike extends Application {
 
@@ -20,9 +24,35 @@ public class Klondike extends Application {
         Game game = new Game();
         game.setTableBackground(new Image("/table/green.png"));
 
+        newButtons(game, primaryStage);
+
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
     }
 
+    private void newButtons(Game game, Stage stage) {
+        Image restartImage = new Image("button_images/restart.png");
+
+        Button restartButton = new Button("");
+        restartButton.setGraphic(new ImageView(restartImage));
+
+        restartButton.setLayoutX(10);
+        restartButton.setLayoutY(10);
+
+        restartButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                restart(stage);
+            }
+        });
+
+        game.getChildren().add(restartButton);
+    }
+
+    private void restart(Stage stage) {
+        start(stage);
+    }
+
 }
+
